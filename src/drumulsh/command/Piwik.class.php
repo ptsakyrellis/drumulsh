b48a7cdfd21ed500a17eb87336e82097e54840b5
ptsakyrellis@github.com <philippe.tsakyrellis@gmail.com>
2017-03-27 11:38:23 +0200

    correction site analytics command

--- src/drumulsh/command/Piwik.class.php
+++ src/drumulsh/command/Piwik.class.php
@@ -1,14 +1,14 @@
 <?php
 
 namespace drumulsh\command;
-/**
- * Cette classe gère la communication avec l'API piwik
- * @author sle-merre
- */
+
 
 class Piwik {
 
-    const URL_PIWIK = 'https://piwik.domain.fr/';
+    /**
+     * API URIS
+     * Constant properties
+     */
     const REQUEST_ADDSITE = 'index.php?module=API&token_auth=%s&method=SitesManager.addSite&siteName=%s&%s';
     const REQUEST_SITEDELETE = 'index.php?module=API&token_auth=%s&method=SitesManager.deleteSite&idSite=%s';
     const REQUEST_GETSITEID = 'index.php?module=API&method=SitesManager.getSitesIdFromSiteUrl&token_auth=%s&url=%s&format=JSON';
@@ -18,31 +18,35 @@ class Piwik {
     const REQUEST_USERGET = 'index.php?module=API&method=UsersManager.getUser&format=JSON&token_auth=%s&userLogin=%s';
     const REQUEST_USERACCESS = 'index.php?module=API&method=UsersManager.setUserAccess&format=JSON&token_auth=%s&userLogin=%s&access=%s&idSites[0]=%s';
     const REQUEST_USERTOKEN = 'index.php?module=API&method=UsersManager.getTokenAuth&format=JSON&token_auth=%s&userLogin=%s&md5Password=%s';
-    const ADMIN_TOKEN = "ADMINSECRETOKEN";
     const ACCESS_VIEW = 'view';
 
-    public static $proxyEnabled = false;
-    public static $proxyHost = 'yourproxy.domain.fr';
-    public static $proxyPort = '9090';
-    public $name;
-    public $username;
-    public $urls;
-    public $siteId;
-    public $inserted = false;
+    /**
+     *
+     */
+    private $piwikUrl;
+    private $domain;
+    private $proxyEnabled = false;
+    private $proxyHost = 'yourproxy.domain.fr';
+    private $proxyPort = '9090';
+    private $name;
+    private $username;
+    private $urls;
+    private $siteId;
+    private $inserted = false;
     private $userExists;
     private $token;
     private $userToken = null;
     private $password = null;
-    private $piwikUrl;
 
     /**
      * 
      * @param string $name Nom de l'établissement (format <type>-<nom>-<commune>)
      * @param array $urlss
      */
-    public function __construct($domain, $directory, $piwikUrl = self::URL_PIWIK, $token = self::ADMIN_TOKEN, $proxyEnabled = false, $proxyHost = null, $proxyPort = null) {
+    public function __construct($domain, $directory, $piwikUrl = 'piwik.local', $token = 'secrettoken', $proxyEnabled = false, $proxyHost = null, $proxyPort = null) {
         //echo "*************************************************************\n";
         //echo "* Piwik API \n";
+        $this->domain = $domain;
         $this->name = '[UAS] - '.$directory;
         $this->username = 'uas_'.$directory;
         $this->token = $token;
@@ -51,9 +55,10 @@ class Piwik {
             sprintf('http://%s/%s', $domain, $directory),
             sprintf('https://%s/%s', $domain, $directory),
         );
-        self::$proxyEnabled = $proxyEnabled;
-        self::$proxyHost = $proxyHost;
-        self::$proxyPort = $proxyPort;
+
+        $this->proxyEnabled = $proxyEnabled;
+        $this->proxyHost = $proxyHost;
+        $this->proxyPort = $proxyPort;
     }
 
     /**
@@ -64,7 +69,7 @@ class Piwik {
         try {
             if (isset($this->siteId))
                 return $this->siteId;
-            $url = sprintf(self::URL_PIWIK . self::REQUEST_GETSITEID, $this->token, urlencode($this->urls[0]));
+            $url = sprintf($this->piwikUrl . self::REQUEST_GETSITEID, $this->token, urlencode($this->urls[0]));
             $result = json_decode($this->request($url));
             if ($result && count($result) === 1) {
                 $this->inserted = true;
@@ -83,7 +88,7 @@ class Piwik {
     function getUser() {
         if (isset($this->user))
             return $this->user;
-        $url = sprintf(self::URL_PIWIK . self::REQUEST_USERGET, $this->token, $this->username);
+        $url = sprintf($this->piwikUrl . self::REQUEST_USERGET, $this->token, $this->username);
         $user = json_decode($this->request($url));
         if ($user && is_array($user) && count($user) === 1) {
             $this->user = $user[0];
@@ -101,7 +106,7 @@ class Piwik {
         }
 
         if ($this->getUser()) {
-            $url = sprintf(self::URL_PIWIK . self::REQUEST_USERTOKEN, urlencode($this->token), urlencode($this->username), md5($this->getPassword()));
+            $url = sprintf($this->piwikUrl . self::REQUEST_USERTOKEN, urlencode($this->token), urlencode($this->username), md5($this->getPassword()));
 
             $sz = $this->request($url);
             $json = json_decode($sz);
@@ -115,13 +120,13 @@ class Piwik {
     }
 
     /**
-     * Crée un utilisateur s'il n'existe pas et le renvoi
+     * Crée un utilisateur s'il n'existe pas et le renvoie
      * @return type
      */
     public function createUser() {
         if (!$this->getUserExists()) {
             echo "Piwik API : Ajout de l'utilisateur au serveur de statistiques\n";
-            $url = sprintf(self::URL_PIWIK . self::REQUEST_USERCREATE, urlencode($this->token), urlencode($this->username), urlencode($this->getPassword()), isset($this->email) ? urlencode($this->email) : urlencode($this->username . '@ac-toulouse.fr')
+            $url = sprintf($this->piwikUrl . self::REQUEST_USERCREATE, urlencode($this->token), urlencode($this->username), urlencode($this->getPassword()), isset($this->email) ? urlencode($this->email) : urlencode($this->username . '@ac-toulouse.fr')
             );
             $creation = json_decode($this->request($url));
             if (!$creation->result)
@@ -135,7 +140,7 @@ class Piwik {
     public function createSiteIfNotExists() {
         if (!$this->getSiteId()) {
             echo "Piwik API : Ajout du site sur le serveur de statistiques\n";
-            $url = sprintf(self::URL_PIWIK . self::REQUEST_ADDSITE, $this->token, urlencode($this->name), $this->getUrlRequestSegment());
+            $url = sprintf($this->piwikUrl . self::REQUEST_ADDSITE, $this->token, urlencode($this->name), $this->getUrlRequestSegment());
             $d = $this->request($url);
         } else {
             echo "Piwik API : Le site existe deja.\n";
@@ -144,10 +149,13 @@ class Piwik {
         return $this->getSiteId();
     }
 
+    /*
+    * Ajoute des droits à l'utilisateur
+    */
     public function addUserAccess() {
         if ($this->getSiteId()) {
             echo "Piwik API : Ajout des droits a l'utilisateur\n";
-            $url = sprintf(self::URL_PIWIK . self::REQUEST_USERACCESS, urlencode($this->token), urlencode($this->username), self::ACCESS_VIEW, $this->getSiteId()
+            $url = sprintf($this->piwikUrl . self::REQUEST_USERACCESS, urlencode($this->token), urlencode($this->username), self::ACCESS_VIEW, $this->getSiteId()
             );
 
             $this->request($url);
@@ -162,17 +170,20 @@ class Piwik {
         if (isset($this->userExists)){
             return $this->userExists;
         }
-        $sz = $this->request(sprintf(self::URL_PIWIK . self::REQUEST_USEREXISTS, $this->token,$this->username));
+        $sz = $this->request(sprintf($this->piwikUrl . self::REQUEST_USEREXISTS, $this->token,$this->username));
 
         $json = json_decode($sz);
         $this->userExists = (bool) $json->value;
         return $this->userExists;
     }
 
+    /*
+    * Suppresion du site sur piwik
+    */
     public function deleteSite() {
         if ($this->getSiteId()) {
             echo "Piwik API : Suppression du site\n";
-            $url = sprintf(self::URL_PIWIK . self::REQUEST_SITEDELETE, $this->token, $this->getSiteId());
+            $url = sprintf($this->piwikUrl . self::REQUEST_SITEDELETE, $this->token, $this->getSiteId());
             $this->request($url);
             return true;
         } else {
@@ -181,11 +192,14 @@ class Piwik {
         return false;
     }
 
+    /*
+    * Suppression de l'utilisateur piwik
+    */
     public function deleteUser() {
         if ($this->getUser()) {
             echo "Piwik API : Suppression de l'utilisateur\n";
             $user = $this->getUser();
-            $url = sprintf(self::URL_PIWIK . self::REQUEST_USERDELETE, $this->token, urlencode($user->login));
+            $url = sprintf($this->piwikUrl . self::REQUEST_USERDELETE, $this->token, urlencode($user->login));
             return $this->request($url);
         } else {
             echo "Piwik API : l'utilisateur n'est pas present dans la base de donnees\n";
@@ -193,6 +207,9 @@ class Piwik {
         return false;
     }
 
+    /*
+     * Suppression du site sur piwik et de l'utilisateur associé
+     */
     public function deleteAll() {
         $this->deleteSite();
         $this->deleteUser();
@@ -208,17 +225,21 @@ class Piwik {
         return $uriSegment;
     }
 
+    /*
+     * Envoie une requête CURL à l'url passée en paramètre
+     * @param $url
+     */
     private function request($url) {
       try {
             $c = curl_init($url);
             curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
             curl_setopt ($c, CURLOPT_SSL_VERIFYHOST, false);
 
-            if (self::$proxyEnabled) {
+            if ($this->proxyEnabled) {
                 $c = curl_init($url);
                 curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
                 curl_setopt($c, CURLOPT_PROXYTYPE, CURLPROXY_HTTP);
-                curl_setopt($c, CURLOPT_PROXY, self::$proxyHost.':'.self::$proxyPort);
+                curl_setopt($c, CURLOPT_PROXY, $this->proxyHost.':'.$this->proxyPort);
             }
 
             $result = curl_exec($c);
@@ -235,6 +256,9 @@ class Piwik {
         return $result;
     }
 
+    /**
+     *  Récupération / Génération du mot de passe utilisateur
+     */
     private function getPassword() {
         if(is_null($this->password)) {
             $this->password = 'MdP!;_'.$this->username.'_';
--- src/drumulsh/command/analyticsSiteCommand.php
+++ src/drumulsh/command/analyticsSiteCommand.php
@@ -9,6 +9,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Input\ArrayInput;
 use actoulouse\dsi\usineasite\Piwik;
 /**
@@ -20,6 +21,10 @@ class analyticsSiteCommand extends Command
     private $sitename = null;
     private $domaine = 'usineasite.local';
     private $analyticsDomain = 'piwik.local';
+    private $adminToken = 'secretoken';
+    private $proxyEnabled = false;
+    private $proxyHost = null;
+    private $proxyPort = '9090';
 
     protected function configure()
     {
@@ -52,14 +57,17 @@ class analyticsSiteCommand extends Command
         $this->sitename = $input->getArgument('sitename');
 
         $output->writeln('<info>Démarrage du setup Piwik...</info>');
-        $piwik = new Piwik($this->domaine, $this->sitename);
 
+
+        $this->setVariables($input, $output);
+
+        // Setup classe piwik avec toutes ces infos
+        $piwik = new \drumulsh\command\Piwik($this->domaine, $this->sitename, $this->analyticsDomain, $this->adminToken, $this->proxyEnabled, $this->proxyHost, $this->proxyPort);
 
         $helper = $this->getHelper('question');
         $question = new ConfirmationQuestion('<question>Vous allez créer un setup piwik pour '.$this->domaine.'/'.$this->sitename.'. Continuer ? (y/n)</question>', false);
 
         if (!$helper->ask($input, $output, $question)) {
-
             return;
         }
 
@@ -118,5 +126,35 @@ class analyticsSiteCommand extends Command
         exec($command);
 
         $output->writeln('<info>Setup piwik terminé, </info>');
+    }
+
+    /**
+     * Questionner l'utilisateur pour initialiser les valeurs des variables
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    private function setVariables(InputInterface $input, OutputInterface $output) {
+        $output->writeln('<info>--------------------------------------</info>');
+        $output->writeln('<info>Ajout d\'un site à la console piwik</info>');
+        $output->writeln('<info>--------------------------------------</info>');
+
+        $helper = $this->getHelper('question');
+        $question = new Question('<question>Entrez l\' API token de l\'administrateur de votre plateforme piwik (défaut: secrettoken) : </question>', 'secrettoken');
+        $this->adminToken = $helper->ask($input, $output, $question);
+
+        $question = new Question('<question>Etes vous derriere un proxy (y/n) ?(défaut: n) : </question>', 'n');
+
+        if($helper->ask($input, $output, $question) == 'y') {
+            $this->proxyEnabled = true;
+        } else {
+            $this->proxyEnabled = false;
+        }
+
+        $question = new Question('<question>Host proxy ?(défaut: null) : </question>', 'null');
+        $this->proxyHost = $helper->ask($input, $output, $question);
+
+        $question = new Question('<question>Port proxy ?(défaut: 8080) : </question>', '9090');
+        $this->proxyPort = $helper->ask($input, $output, $question);
     }
 }
